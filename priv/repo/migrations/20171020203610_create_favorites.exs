db62051521c6c8b41b3f9c49c0090c9ad376734d
Cairo Noleto <caironoleto@gmail.com>
2017-10-20 19:34:48 -0200

    Add RealWorld.Blog.Favorite schema.

---
+++ priv/repo/migrations/20171020203610_create_favorites.exs
@@ -0,0 +1,13 @@
+defmodule RealWorld.Repo.Migrations.CreateFavorites do
+  use Ecto.Migration
+
+  def change do
+    create table(:favorites) do
+      add :user_id, references(:users), primary_key: true
+      add :article_id, references(:articles), primary_key: true
+
+      timestamps()
+    end
+
+  end
+end
---
+++ test/real_world/favorite_test.exs
@@ -0,0 +1,41 @@
+defmodule RealWorld.Blog.FavoriteTest do
+  @moduledoc false
+  use ExUnit.Case
+  use RealWorld.DataCase
+  alias RealWorld.Blog.Favorite
+  import RealWorld.Factory
+
+  describe "schema" do
+    test "schema metadata" do
+      assert Favorite.__schema__(:source) == "favorites"
+      assert Favorite.__schema__(:fields) == [
+        :id, :user_id, :article_id, :inserted_at, :updated_at
+      ]
+      assert Favorite.__schema__(:primary_key) == [:id]
+    end
+  end
+
+  test "changeset/2 with valida params" do
+    favorite = %Favorite{}
+    user = insert(:user)
+    article = insert(:article, author: user)
+
+    params = %{
+      "user_id" => user.id,
+      "article_id" => article.id
+    }
+
+    changes = %{
+      user_id: user.id,
+      article_id: article.id
+    }
+
+    changeset = Favorite.changeset(favorite, params)
+    assert changeset.params == params
+    assert changeset.data == favorite
+    assert changeset.changes == changes
+    assert changeset.validations == []
+    assert changeset.required == [:user_id, :article_id]
+    assert changeset.valid?
+  end
+end
