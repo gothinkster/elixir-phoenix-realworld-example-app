5e6054809ad70db4fbbb0e88407412e4c70766e3
Leandro Bighetti <lbighetti@gmail.com>
2019-04-10 23:38:31 +0200

    Starts using Semantic Versioning
    - Tagging it v1.0.0 as the start of semver usage
    - Adds a CHANGELOG.md based on http://keepachangelog.com/en/1.0.0/

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RealWorld.Mixfile do
   def project do
     [
       app: :real_world,
-      version: "0.0.1",
+      version: "1.0.0",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
