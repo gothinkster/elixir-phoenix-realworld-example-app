9655779aa540e1aa6ce8f577cc073b4294df8076
Steven Blowers <sb@rekki.com>
2019-10-09 12:40:26 +0100

    🐛 fix bug where password not hashed on update

--- lib/real_world/accounts/users.ex
+++ lib/real_world/accounts/users.ex
@@ -13,6 +13,7 @@ defmodule RealWorld.Accounts.Users do
   def update_user(user, attrs) do
     user
     |> User.changeset(attrs)
+    |> RealWorld.Accounts.Auth.hash_password()
     |> Repo.update()
   end
 
