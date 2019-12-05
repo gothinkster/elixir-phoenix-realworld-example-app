24b2fd28c8000c098e8cc3e277dd8a50a5ad240f
Steven Blowers <sb@rekki.com>
2019-12-05 10:50:41 +0000

    upgrade to elixir 1.9

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule RealWorld.Mixfile do
     [
       app: :real_world,
       version: "1.0.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
