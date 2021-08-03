0fbd9704ef5e7f0e89efdc8b5fd6ca06d4e617c5
Vinicius Silveira <viniciussilveira6@gmail.com>
2021-08-03 01:04:21 -0300

    add new tests to auth context

--- test/real_world/auth_test.exs
+++ test/real_world/auth_test.exs
@@ -1,23 +1,77 @@
 defmodule RealWorld.Accounts.AuthTest do
   @moduledoc false
   use RealWorld.DataCase
+  import RealWorld.Factory
 
-  alias RealWorld.Accounts.Auth
+  alias RealWorld.Accounts.{Auth, User}
 
-  @user_create_attrs %{
-    email: "some email",
-    password: "some password",
-    username: "some username",
-    bio: "some bio",
-    image: "some image"
-  }
+  setup do
+    user_params = params_for(:user)
+    {:ok, %User{} = user} = Auth.register(user_params)
 
-  test "register/1 creates a user" do
-    Auth.register(@user_create_attrs)
+    {:ok, user: user, user_params: user_params}
   end
 
-  test "register/1 returns error if email is used already" do
-    Auth.register(@user_create_attrs)
-    assert {:error, _} = Auth.register(@user_create_attrs)
+  describe "register/1" do
+    test "creates a user", %{user: user, user_params: user_params} do
+      assert user.username == user_params.username
+      assert user.email == user_params.email
+      assert user.image == user_params.image
+      assert user.bio == user_params.bio
+    end
+
+    test "returns error when email params does not exist", %{user_params: user_params} do
+      assert {:error, %Ecto.Changeset{} = changeset} =
+               user_params
+               |> Map.delete(:email)
+               |> Auth.register()
+
+      assert "can't be blank" in errors_on(changeset).email
+    end
+
+    test "returns error when username params does not exist", %{user_params: user_params} do
+      assert {:error, %Ecto.Changeset{} = changeset} =
+               user_params
+               |> Map.delete(:username)
+               |> Auth.register()
+
+      assert "can't be blank" in errors_on(changeset).username
+    end
+
+    test "register/1 returns error if username is used already", %{user_params: user_params} do
+      assert {:error, %Ecto.Changeset{} = changeset} = Auth.register(user_params)
+
+      assert "has already been taken" in errors_on(changeset).username
+    end
+
+    test "register/1 returns error if email is used already", %{user_params: user_params} do
+      assert {:error, %Ecto.Changeset{} = changeset} =
+               user_params
+               |> Map.put(:username, "another name")
+               |> Auth.register()
+
+      assert "has already been taken" in errors_on(changeset).email
+    end
+  end
+
+  describe "find_user_and_check_password/1" do
+    test "when user found", %{user_params: user_params} do
+      assert {:ok, %User{} = user} =
+               Auth.find_user_and_check_password(%{
+                 "user" => %{"email" => user_params.email, "password" => user_params.password}
+               })
+
+      assert user.username == user_params.username
+      assert user.email == user_params.email
+      assert user.image == user_params.image
+      assert user.bio == user_params.bio
+    end
+
+    test "when user not found" do
+      assert {:error, "Could not login"} ==
+               Auth.find_user_and_check_password(%{
+                 "user" => %{"email" => "any@mail.com", "password" => "any passwd"}
+               })
+    end
   end
 end
